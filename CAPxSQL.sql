293d6f114533e18e34bcb432934f3464b5091db2
AlexNeumann <alex.neumann92@gmail.com>
2015-03-10 19:37:53 -0700

    Added Release 0.4 files

--- CAPxSQL.sql
+++ CAPxSQL.sql
@@ -4,23 +4,48 @@ DROP TABLE IF EXISTS PROJECT2 CASCADE;
 
 CREATE TABLE PROJECT2 (
 	id		serial		NOT NULL		PRIMARY KEY,
-	company		varchar(50),
-	description	varchar(300),
-	comments varchar(300),
-	rating smallint)
+	company				varchar(50),
+	description			varchar(300),
+	comments 			varchar(300),
+	rating 				smallint,
+	tags 				varchar[20],
+	dateCreated			date,
+	semester			varchar(20),
+	contact				varchar(60),
+	email				varchar(100),
+	phone				varchar(20),
+	active				varchar(3))
 	
-INSERT INTO PROJECT2 VALUES (1, 'Spark Open Research, LLC', 'Develop student user analytics...', 'Project 1. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse enim justo, molestie in justo sed, malesuada euismod erat. Nam suscipit sed turpis convallis pulvinar. Cras gravida elit nisl, sit amet sodales erat molestie lacinia.', 5);
-INSERT INTO PROJECT2 VALUES (2, 'Aspen Systems, Inc.', 'The project presented by Aspen Systems is a native Android...', 'Project 2. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse enim justo, molestie in justo sed, malesuada euismod erat. Nam suscipit sed turpis convallis pulvinar. Cras gravida elit nisl, sit amet sodales erat molestie lacinia.', 6);
-INSERT INTO PROJECT2 VALUES (3, 'ABC Company, LLC', 'Develop a website for an online retailer...', 'Project 3. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse enim justo, molestie in justo sed, malesuada euismod erat. Nam suscipit sed turpis convallis pulvinar. Cras gravida elit nisl, sit amet sodales erat molestie lacinia.', 2);
-INSERT INTO PROJECT2 VALUES (4, 'XYZ Clothin, Inc', 'Design a sales platform for a leading clothing business...', 'Project 4. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse enim justo, molestie in justo sed, malesuada euismod erat. Nam suscipit sed turpis convallis pulvinar. Cras gravida elit nisl, sit amet sodales erat molestie lacinia.', 0);
+	
+INSERT INTO PROJECT2 (id, company, description, comments, rating) 
+VALUES (1, 'Spark Open Research, LLC', 'Develop student user analytics and reporting on the Open edX platform. In this project, students will have the opportunity to develop open-source tools for educators and education researchers to allow them to better understand how students learn.', 'Project 1. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse enim justo, molestie in justo sed, malesuada euismod erat. Nam suscipit sed turpis convallis pulvinar. Cras gravida elit nisl, sit amet sodales erat molestie lacinia.', 5);
+INSERT INTO PROJECT2 (id, company, description, comments, rating) 
+VALUES (2, 'Aspen Systems, Inc.', 'The project presented by Aspen Systems is a native Android application used to help delivery truck drivers view customer information, view route information, allow customers to sign off on deliveries and other related actions.', 'Project 2. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse enim justo, molestie in justo sed, malesuada euismod erat. Nam suscipit sed turpis convallis pulvinar. Cras gravida elit nisl, sit amet sodales erat molestie lacinia.', 6);
+INSERT INTO PROJECT2 (id, company, description, comments, rating) 
+VALUES (3, 'ABC Company, LLC', 'Develop a website for an online retailer. Students will be expected to create an Android and iOS mobile app in order to let customers purchase products from their phone. A shopping cart will be required.', 'Project 3. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse enim justo, molestie in justo sed, malesuada euismod erat. Nam suscipit sed turpis convallis pulvinar. Cras gravida elit nisl, sit amet sodales erat molestie lacinia.', 2);
+INSERT INTO PROJECT2 (id, company, description, comments, rating) 
+VALUES (4, 'XYZ Clothin, Inc', 'Design a sales platform for a leading clothing business. Students will have the opportunity to visit our local factory and the project will require extensive front-end design to make our new website express our company style.', 'Project 4. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse enim justo, molestie in justo sed, malesuada euismod erat. Nam suscipit sed turpis convallis pulvinar. Cras gravida elit nisl, sit amet sodales erat molestie lacinia.', 0);
 
-
+DROP TABLE IF EXISTS USERS CASCADE;
 
 CREATE TABLE USERS (
 	usertoken		varchar(100)		NOT NULL		PRIMARY KEY,
 	votingArray		integer[300],
 	githubname 		varchar(50),
-	placeholder1	varchar(50),
+	lastlogindate	date,
+	lastlogintime	time,
+	accountType		varchar(15)		default 'normal',
 	placeholder2 	varchar(50),
 	placeholder3 	smallint)
+	
+	
+Drop TABLE IF EXISTS BLOG CASCADE;
+	
+CREATE TABLE BLOG (
+	id		serial		NOT NULL		PRIMARY KEY,
+	blogtitle		varchar(100),
+	blogentry		varchar(1000),
+	blogwriter		varchar(50),
+	dateCreated		date,
+	timeCreated		time)
 	
--- app.py
+++ app.py
@@ -1,18 +1,29 @@
 import psycopg2, requests, json
 from flask.ext.github import GitHub
-from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask import Flask, render_template, request, redirect, url_for, flash, session, g
+import datetime
+
 
 #setup the database connection and create 3 cursor objects to execute SQL statements
-conn = psycopg2.connect(database="db", user="user", password="pw", host="host", port="port")
-cur = conn.cursor()
-cur2 = conn.cursor()
-cur3 = conn.cursor()
+#conn = psycopg2.connect(database="da0c336s7ha473", user="jzmjrrvrslorle", password="uzRtSKdmsmv2KxDpc63eGr7VYd", host="ec2-107-22-234-129.compute-1.amazonaws.com", port="5432")
+#cur = conn.cursor()
+#cur2 = conn.cursor()
+#cur3 = conn.cursor()
 
+def connect_db():
+	g.conn = psycopg2.connect(database="MODIFY", user="MODIFY", password="MODIFY", host="MODIFY", port="MODIFY")
+	g.cur = g.conn.cursor()
+	
+def close_db():
+	g.conn.close()
+	g.cur.close()
+	
+	
 #setup for GitHub login using Client ID and Client Secret
 app = Flask(__name__)
-app.config['GITHUB_CLIENT_ID'] = 'clientId'
-app.config['GITHUB_CLIENT_SECRET'] = 'ClientSecret'
-app.secret_key = 'yourSecretKey'
+app.config['GITHUB_CLIENT_ID'] = 'MODIFY'
+app.config['GITHUB_CLIENT_SECRET'] = 'MODIFY'
+app.secret_key = 'MODIFY'
 
 github = GitHub(app)
 
@@ -23,6 +34,8 @@ showroomStatus = ''
 aboutStatus = ''
 projectStatus = ''
 homeStatus = ''
+addprojectStatus = ''
+adminStatus = ''
 
 # This variable is passed to every child template to determine which Navigation bar they should extend (logged in or not logged in)
 logincheck = ''
@@ -34,35 +47,48 @@ logincheck = ''
 # Index route
 @app.route('/')
 def index():
+	connect_db()
 	# Sets the current users GitHub name
 	if 'userName' in session:
 		print('Do nothing')
 	else:
 		session['userName'] = ''
+		session['adminCheck'] = ''
 		# Tracks if a user is logged in or not
 		session['blnLoggedIn'] = ''
 		#Tracks the user's oauth code provided through GitHub login
-		session['oauth'] = ''
+		session['oauth'] = ''		
+	
 	homeStatus = 'active1'
 	if session['blnLoggedIn'] == "yes":
 		logincheck = '_loggedin'
 	else:
 		logincheck = ''
-	return render_template("index.html", homeStatus=homeStatus, logincheck=logincheck, userName=session['userName'])
+	
+
+	# pull all blog posts, show the most recent
+	g.cur.execute("select * from Blog order by datecreated desc, timecreated desc;")
+	blogs = g.cur.fetchall()
+	
+	close_db()
+
+	return render_template("index.html", homeStatus=homeStatus, logincheck=logincheck, admin=session['adminCheck'], userName=session['userName'], blogs=blogs)
 
 # Projects route
 @app.route('/projects', methods = ['GET', 'POST'])
 def project():
+	connect_db()
 	# select all projects from the project2 table and order them (highest rating first)
 	# pass SQL result to project page
-	cur.execute("SELECT * FROM project2 ORDER BY rating DESC;")
-	rows = cur.fetchall()
+	g.cur.execute("SELECT * FROM project2 where active = 'yes' ORDER BY rating DESC;")
+	rows = g.cur.fetchall()
 	projectStatus = 'active1'
 	if session['blnLoggedIn'] == "yes":
 		logincheck = '_loggedin'
 	else:
 		logincheck = ''
-	return render_template("project.html", rows=rows, cur2=cur2, conn=conn, projectStatus=projectStatus, logincheck=logincheck, userName=session['userName'])
+	close_db()
+	return render_template("project.html", rows=rows, projectStatus=projectStatus, logincheck=logincheck, userName=session['userName'], admin=session['adminCheck'])
 
 # Showroom route	
 @app.route('/showroom')
@@ -72,7 +98,7 @@ def showroom():
 		logincheck = '_loggedin'
 	else:
 		logincheck = ''
-	return render_template("showroom.html", showroomStatus=showroomStatus, logincheck=logincheck, userName=session['userName'])
+	return render_template("showroom.html", showroomStatus=showroomStatus, logincheck=logincheck, userName=session['userName'], admin=session['adminCheck'])
 
 # About route
 @app.route('/about')
@@ -82,7 +108,17 @@ def about():
 		logincheck = '_loggedin'
 	else:
 		logincheck = ''
-	return render_template("about.html", aboutStatus=aboutStatus, logincheck=logincheck, userName=session['userName'])
+	return render_template("about.html", aboutStatus=aboutStatus, logincheck=logincheck, userName=session['userName'], admin=session['adminCheck'])
+	
+# Add Project route
+@app.route('/addproject')
+def addproject():
+	addprojectStatus = 'active1'
+	if session['blnLoggedIn'] == "yes":
+		logincheck = '_loggedin'
+	else:
+		logincheck = ''
+	return render_template("addproject.html", addprojectStatus=addprojectStatus, logincheck=logincheck, userName=session['userName'], admin=session['adminCheck'])
 	
 # Login route - calls the GitHub authorize function
 @app.route('/login')
@@ -92,57 +128,71 @@ def login():
 # Logout route
 @app.route('/logout')
 def logout():
-	session['blnLoggedIn'] = ''
-	session['userName'] = ''
-	return render_template("logout.html")
+	session.pop('userName', None)
+	session.clear()
+	flash('You successfully logged out!')
+	return redirect(url_for('index', _external=True, _scheme='https'))
 	
+# Admin route
+@app.route('/adminpanel')
+def adminpanel():
+	if session['adminCheck'] != 'yes':
+		return redirect(url_for('index', _external=True, _scheme='https'))
+	adminStatus = 'active1'
+	if session['blnLoggedIn'] == "yes":
+		logincheck = '_loggedin'
+	else:
+		logincheck = ''
+	return render_template("adminpanel.html", adminStatus=adminStatus, logincheck=logincheck, userName=session['userName'], admin=session['adminCheck'])
+
+
 # Welcome route 
 # This route is executed once users log in with GitHub
 @app.route('/welcome', methods = ['GET', 'POST'])
 @github.authorized_handler
 def authorized(oauth_token): 
+	connect_db()
 	session['oauth'] = oauth_token
-	# check if current user is already in database with the corresponding token
-	cur2.execute("SELECT usertoken FROM users WHERE usertoken= %(oauth)s;", {'oauth': session['oauth']})
-	user = cur2.fetchone()
+	j = requests.get('https://api.github.com/user?access_token=' + session['oauth'])
+	j = json.loads(j.content.decode('utf-8'))
+	session['userName'] = j['login']
+	
+	
+	
+	
+	# if session['userName'] == 'AlexNeumann':
+		# session['adminCheck'] = 'yes'
+	# if session['userName'] == 'STruong1':
+		# session['adminCheck'] = 'yes'
+	
+	# check if current user is already in database with the corresponding githubname
+	g.cur.execute("SELECT githubname FROM users WHERE githubname= %(user)s;", {'user': session['userName']})
+	user = g.cur.fetchone()
 
 	# create new user record if user was not found
 	if user is None:
-		cur2.execute("INSERT into users VALUES (%(oauth)s)", {'oauth': session['oauth']})
-		conn.commit()	
-	
-	# Following code is used for parsing out the GitHub username from the returned JSON object
-	j = requests.get('https://api.github.com/user?access_token=' + session['oauth'])
-	j = j.text
-
-	i = 0
-	text = ""
-	quoteCount = 0
-	beginning = 0
-	end = 0
-
-	while quoteCount < 4:
-		text = text + j[i]
-		if j[i] == '"':
-			quoteCount = quoteCount + 1
-		if quoteCount == 3 and j[i] == '"':
-			beginning = i
-		if quoteCount == 4:
-			end = i
-		i = i + 1
-
-	while beginning < end - 1:
-		session['userName'] = session['userName'] + (j[beginning+1])
-		beginning = beginning + 1
-	
-	# Add the GitHub name to the record matching the oauth token
-	cur2.execute("UPDATE users SET githubname =  %(gitname)s WHERE usertoken = %(oauth)s;", {'gitname': session['userName'], 'oauth': session['oauth']})
-	conn.commit()
+		g.cur.execute("INSERT into users (usertoken, githubname) VALUES (%(oauth)s, %(user)s)", {'oauth': session['oauth'], 'user': session['userName']})
+		g.conn.commit()	
+	else:
+		g.cur.execute("UPDATE users SET usertoken =  %(oauth)s  WHERE githubname = %(user)s;", {'oauth': session['oauth'], 'user': session['userName']})
+		g.conn.commit()	
+		
+	currentDate = datetime.date.today()
+	g.cur.execute("UPDATE users SET lastlogindate = %(date)s WHERE githubname = %(user)s;", {'date': currentDate, 'user': session['userName']})
+	g.conn.commit()
 	
 	# Set logged in status to yes
 	session['blnLoggedIn'] = 'yes'
 	
-	return render_template("welcome.html", userName=session['userName'])
+	# temporary solution to check if the user is an admin. Should be switched to DB conn line
+	g.cur.execute("SELECT accountType FROM users WHERE githubname = %(user)s", {'user': session['userName']})
+	result = g.cur.fetchone()
+	
+	if result[0] == 'admin':
+		session['adminCheck'] = 'yes'
+	
+	close_db()
+	return redirect(url_for('index', _external=True, _scheme='https'))
 	
 # Upvote route
 # This route is executed whenever a user upvotes a project on the Project page
@@ -152,27 +202,36 @@ def upvote():
 	if session['blnLoggedIn'] == '':
 		return render_template("pleaselogin.html")
 	else:	
+		connect_db()
 		# The corresponding HTML form posts the ID number for the project that was upvoted
 		projectId = request.form.get('upvote', None)
 		projectId = int(projectId)
 		projectId = projectId - 1
-		oauthvote = session['oauth']
+		oauthvote = session['userName']
 		# Get the votingArray from the user table for the currently logged in user
-		cur.execute("SELECT votingArray[%(indexof)s] FROM users WHERE usertoken = %(oauth)s;", {'oauth': oauthvote, 'indexof': projectId})
-		voteArray = cur.fetchone()
+		g.cur.execute("SELECT votingArray[%(indexof)s] FROM users WHERE githubname = %(oauth)s;", {'oauth': oauthvote, 'indexof': projectId})
+		voteArray = g.cur.fetchone()
 		
 		# If the array is already set to 1, then the user has already voted for this specific project
 		if voteArray[0] == 1:
-			flash('You already voted for this project!')
+			flash('You already upvoted this project!')
+		elif voteArray[0] == -1:
+			g.cur.execute("UPDATE users SET votingArray[%(indexof)s] = 0 WHERE githubname = %(oauth)s;", {'oauth': oauthvote, 'indexof': projectId})
+			g.conn.commit()
+			projectId = projectId + 1
+			g.cur.execute("update project2 set rating = rating + 1 where id = %(id)s;", {'id': projectId})
+			g.conn.commit()
+			flash('You now have a neutral vote for this project!')
 		# If array is not set to 1, it means the user is allowed to vote for this project
 		else:
 			# Set the votingArray element to 1 and increment the rating for the appropriate project by 1
-			cur3.execute("UPDATE users SET votingArray[%(indexof)s] = 1 WHERE usertoken = %(oauth)s;", {'oauth': oauthvote, 'indexof': projectId})
-			conn.commit()
+			g.cur.execute("UPDATE users SET votingArray[%(indexof)s] = 1 WHERE githubname = %(oauth)s;", {'oauth': oauthvote, 'indexof': projectId})
+			g.conn.commit()
 			projectId = projectId + 1
-			cur2.execute("update project2 set rating = rating + 1 where id = %(id)s;", {'id': projectId})
-			conn.commit()
+			g.cur.execute("update project2 set rating = rating + 1 where id = %(id)s;", {'id': projectId})
+			g.conn.commit()
 			flash('You successfully voted for this project!')
+		close_db()
 		return redirect('/projects')
 
 # Downvote route
@@ -183,24 +242,227 @@ def downvote():
 	if session['blnLoggedIn'] == '':
 		return render_template("pleaselogin.html")
 	else:	
+		connect_db()
 		projectId = request.form.get('downvote', None)
 		projectId = int(projectId)
 		projectId = projectId - 1
-		oauthvote = session['oauth']
-		cur.execute("SELECT votingArray[%(indexof)s] FROM users WHERE usertoken = %(oauth)s;", {'oauth': oauthvote, 'indexof': projectId})
-		voteArray = cur.fetchone()
+		oauthvote = session['userName']
+		g.cur.execute("SELECT votingArray[%(indexof)s] FROM users WHERE githubname = %(oauth)s;", {'oauth': oauthvote, 'indexof': projectId})
+		voteArray = g.cur.fetchone()
 		
-		if voteArray[0] == 1:
-			flash('You already voted for this project!')
-		else:
-			cur3.execute("UPDATE users SET votingArray[%(indexof)s] = 1 WHERE usertoken = %(oauth)s;", {'oauth': oauthvote, 'indexof': projectId})
-			conn.commit()
+		if voteArray[0] == -1:
+			flash('You already downvoted this project!')
+		elif voteArray[0] == 1:
+			g.cur.execute("UPDATE users SET votingArray[%(indexof)s] = 0 WHERE githubname = %(oauth)s;", {'oauth': oauthvote, 'indexof': projectId})
+			g.conn.commit()
 			projectId = projectId + 1
-			cur2.execute("update project2 set rating = rating - 1 where id = %(id)s;", {'id': projectId})
-			conn.commit()
+			g.cur.execute("update project2 set rating = rating - 1 where id = %(id)s;", {'id': projectId})
+			g.conn.commit()
+			flash('You now have a neutral vote for this project!')
+		else:
+			g.cur.execute("UPDATE users SET votingArray[%(indexof)s] = -1 WHERE githubname = %(oauth)s;", {'oauth': oauthvote, 'indexof': projectId})
+			g.conn.commit()
+			projectId = projectId + 1
+			g.cur.execute("update project2 set rating = rating - 1 where id = %(id)s;", {'id': projectId})
+			g.conn.commit()
 			flash('You successfully voted for this project!')
-
+		close_db()
 		return redirect('/projects')
+		
+# Propose project route		
+@app.route('/proposeproject', methods = ['POST', 'GET'])
+def proposeproject():
+	firstName = request.form.get('firstname', None)
+	lastName = request.form.get('lastname', None)
+	email = request.form.get('email', None)
+	phone = request.form.get('phone', None)
+	company = request.form.get('company', None)
+	projectTitle = request.form.get('project', None)
+	description = request.form.get('comment', None)
+	
+	htmlbox = request.form.get('HTML', None)
+	cssbox = request.form.get('CSS', None)
+	javabox = request.form.get('Java', None)
+	pythonbox = request.form.get('Python', None)
+	csharpbox = request.form.get('CSharp', None)
+	djangobox = request.form.get('Django', None)
+	iosmobilebox = request.form.get('iOSMobile', None)
+	androidbox = request.form.get('Android', None)
+	
+	tags = ''
+	
+	if htmlbox == 'on':
+		tags = tags + "HTML,"
+	if cssbox == 'on':
+		tags = tags + "CSS,"
+	if javabox == 'on':
+		tags = tags + "Java,"
+	if pythonbox == 'on':
+		tags = tags + "Python,"
+	if csharpbox == 'on':
+		tags = tags + "C#,"
+	if djangobox == 'on':
+		tags = tags + "Django,"
+	if iosmobilebox == 'on':
+		tags = tags + "iOS Mobile,"
+	if androidbox == 'on':
+		tags = tags + "Android,"
+		
+	tags = tags[:-1]
+	tags = "{" + tags + "}"
+
+	
+	currentDate = date = datetime.date.today()
+	semester = 'Spring2015'
+	contact = firstName + ' ' + lastName
+	
+	print (firstName)
+	print (lastName)
+	print (email)
+	print (phone)
+	print (company)
+	print (projectTitle)
+	print (description)
+	
+	connect_db()
+	g.cur.execute("INSERT INTO PROJECT2 (company, description, rating, tags, dateCreated, semester, contact, email, phone, active) VALUES (%(company)s, %(description)s, 0, %(tags)s, %(date)s, %(semester)s, %(contact)s, %(email)s, %(phone)s, 'no');", {'company': company, 'description': description, 'tags': tags, 'date': currentDate, 'semester': semester, 'contact': contact, 'email': email, 'phone': phone})
+	g.conn.commit()
+	close_db()
+	return redirect(url_for('index', _external=True, _scheme='https'))
+
+# Admin Feature: Blog Main Page
+@app.route('/adminBlog')
+def adminBlog():
+	if session['adminCheck'] != 'yes':
+		return redirect(url_for('index', _external=True, _scheme='https'))
+	adminStatus = 'active1'
+	if session['blnLoggedIn'] == "yes":
+		logincheck = '_loggedin'
+	else:
+		logincheck = ''
+	return render_template("adminBlog.html", adminStatus=adminStatus, logincheck=logincheck, userName=session['userName'], admin=session['adminCheck'])	
+	
+# Admin Feature: Blog entry
+@app.route('/blogentry', methods = ['POST', 'GET'])		
+def blogentry():	
+	if session['adminCheck'] != 'yes':
+		return redirect(url_for('index', _external=True, _scheme='https'))
+	blogtitle = request.form.get('blogtitle', None)
+	blogpost = request.form.get('blogpost', None)
+	
+	currentDate = datetime.date.today()
+	time = datetime.datetime.now()
+	hour = time.hour
+	minute = time.minute
+	timeCreated = str(hour) + ':' + str(minute)
+	
+	blogwriter = ''
+	blogwriter = session['userName']
+	
+	connect_db()
+	g.cur.execute("INSERT INTO BLOG (blogtitle, blogentry, blogwriter, dateCreated, timeCreated) VALUES (%s, %s, %s, %s, %s);", (blogtitle, blogpost, blogwriter, currentDate, timeCreated))
+	g.conn.commit()
+	close_db()
+	flash('You successfully submitted a blog entry!')
+	return redirect('/adminBlog')
+	
+# Admin Feature: Project approval Main Page
+@app.route('/adminProject')
+def adminProject():
+	if session['adminCheck'] != 'yes':
+		return redirect(url_for('index', _external=True, _scheme='https'))
+	connect_db()
+	adminStatus = 'active1'
+	if session['blnLoggedIn'] == "yes":
+		logincheck = '_loggedin'
+	else:
+		logincheck = ''
+	
+	g.cur.execute("SELECT * from project2 WHERE active = 'no' ORDER BY dateCreated desc;")
+	pending = g.cur.fetchall()
+	close_db()
+	return render_template("adminProject.html", adminStatus=adminStatus, logincheck=logincheck, userName=session['userName'], admin=session['adminCheck'], pending=pending)
+	
+# Admin Feature: Approve and Add Comment toProject
+@app.route('/addcomment', methods = ['POST', 'GET'])		
+def addcomment():	
+	if session['adminCheck'] != 'yes':
+		return redirect(url_for('index', _external=True, _scheme='https'))
+	projectid = request.form.get('commentid', None)
+	comment = request.form.get('addedcomment', None)
+	comment = str(comment)
+	print (comment)
+	print (projectid)
+	
+	connect_db()
+	g.cur.execute("UPDATE project2 SET active = 'yes', comments = %s WHERE id = %s;", (comment, projectid))
+	g.conn.commit()
+	close_db()
+	flash('You successfully approved a project!')
+	return redirect('/adminProject')
+	
+# Admin Feature: Promote Admin Main Page
+@app.route('/adminPromote', methods = ['POST', 'GET'])
+def adminPromote():
+	if session['adminCheck'] != 'yes':
+		return redirect(url_for('index', _external=True, _scheme='https'))
+	connect_db()
+	adminStatus = 'active1'
+	if session['blnLoggedIn'] == "yes":
+		logincheck = '_loggedin'
+	else:
+		logincheck = ''
+	g.cur.execute("SELECT githubname from users WHERE accountType = 'normal';")
+	normUser = g.cur.fetchall()
+	close_db()	
+	
+	return render_template("adminPromote.html", adminStatus=adminStatus, logincheck=logincheck, userName=session['userName'], admin=session['adminCheck'], normUser=normUser)
+		
+# Admin Feature: Promote Account to Admin
+@app.route('/createAdmin', methods = ['POST', 'GET'])
+def createAdmin():
+	if session['adminCheck'] != 'yes':
+		return redirect(url_for('index', _external=True, _scheme='https'))
+	githubname = request.form.get('createAdmin', None)
+	print ('Trying to promote...' + githubname)
+	
+	connect_db()
+	g.cur.execute("SELECT githubname from users where accountType = 'normal';")
+	normUser = g.cur.fetchall()
+
+	nameExists = 0
+	for row in normUser:
+		if githubname == row[0]:
+			nameExists = 1
+	if nameExists == 1:
+		g.cur.execute("UPDATE users SET accountType = 'admin' WHERE githubname = %(name)s;", {'name':githubname})
+		g.conn.commit()
+		close_db()
+		print('Successfully promoted ' + githubname)
+		flash('Successfully promoted ' + githubname + ' to admin!')
+	else:
+		flash('No record with this GitHub name exists. Please try again.')
+		close_db()
+		
+	return redirect('/adminPromote')
+
+# Admin Feature: Promote Admin Main Page
+@app.route('/adminRoster', methods = ['POST', 'GET'])
+def adminRoster():
+	if session['adminCheck'] != 'yes':
+		return redirect(url_for('index', _external=True, _scheme='https'))
+	connect_db()
+	adminStatus = 'active1'
+	if session['blnLoggedIn'] == "yes":
+		logincheck = '_loggedin'
+	else:
+		logincheck = ''
+	g.cur.execute("SELECT githubname, lastlogindate, accountType from users;")
+	roster = g.cur.fetchall()
+	close_db()	
+	
+	return render_template("adminRoster.html", adminStatus=adminStatus, logincheck=logincheck, userName=session['userName'], admin=session['adminCheck'], roster=roster)
+	
 	
 	
 if __name__ == '__main__':
